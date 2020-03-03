825012789c99e31d84f3aed98e5fb678474c4bf0
Taron Abgaryan <taronabg77@gmail.com>
2020-03-03 19:45:49 +0400

    code review

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -9,19 +9,16 @@ import { ScrollService } from '../../services/scroll.service'
 export class NavBarComponent implements OnInit { 
   public activeRout: string = "about"
   public scrollTop: number = 0;
-  public windowWidth: number = window.outerWidth;
 
   constructor(private scrollService: ScrollService) { 
     this.scrollService.onScroll$().subscribe(scrollTop => {
       this.scrollTop = scrollTop;
-      this.windowWidth = window.outerWidth;
-    }
-    )
+    })
   }
 
   ngOnInit() {
-
   }
+  
   changActiveRout(raout) {
     this.activeRout = raout;
   }
